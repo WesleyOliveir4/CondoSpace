527edf978e809b3ca5970b5675e4f814e5105d59
wesley oliveira <wlwwesley9@gmail.com>
2026-03-19 22:18:46 -0300

    fix: navbar publish

--- app/src/main/java/com/example/condospace/ui/feature/publish/PublishScreen.kt
+++ app/src/main/java/com/example/condospace/ui/feature/publish/PublishScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -27,11 +28,11 @@ fun PublishScreen(navController: NavHostController) {
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PublishScreenContent(navController: NavHostController) {
     Scaffold(
-        bottomBar = { NavBar(navController, "Publicar") },
+        bottomBar = { NavBar(navController, "Publish") },
         topBar = {
             CondoSpaceTopBar()
         }
